72e025a2559a4fa498730cab0a214fcc632663fc
Samuel <samuelsiuyanyat@gmail.com>
2024-06-16 22:23:02 +0800

    separate cors domain env from client url

--- express.js
+++ express.js
@@ -69,7 +69,7 @@ app.use(session({
 
 // for allowing connection with frontend?
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", clientUrl);
+    res.setHeader("Access-Control-Allow-Origin", process.env.cors_domain);
     res.set("Access-Control-Allow-Credentials", 'true')
     next();
 });
